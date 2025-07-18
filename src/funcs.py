d6065e0612faa86f8ce9148f8d16d0c01a41f085
jarvink7 <karri.a.jarvinen@gmail.com>
2025-07-18 08:48:02 +0300

    Ditched DearPyGUI in favor of PyQt6 since Panda3D and PyQt both use OpenGL making integration a whole lot easier.

--- src/funcs.py
+++ src/funcs.py
@@ -13,52 +13,38 @@ import numpy as np
 import random
 
 
-def startStopSimulation(self):
-    if self.pause_flag:
-        self.pause_flag = False
+def startStopSimulation(panda):
+    if panda.pause_flag:
+        panda.pause_flag = False
         # Re-add the moveAtomsTask to task manager
-        self.taskMgr.add(self.moveAtomsTask, "MoveAtomsTask")
+        panda.taskMgr.add(panda.moveAtomsTask, "MoveAtomsTask")
     else:
-        self.pause_flag = True
+        panda.pause_flag = True
 
-def changeSpeed(self):
-    self.timestep = self.animTime_slider["value"]
+def changeSpeed(panda, v):
+    panda.timestep = v
 
-def changeThermo(self):
+def changeThermo(panda, v):
     # Change thermal endpoint and update fix
-    self.tStop = 2**self.thermo_slider["value"]
-    self.lmp.command(f"fix 2 all langevin {self.tStart} {self.tStop} 0.1 102938")
+    panda.tStop = 2**v
+    panda.lmp.command(f"fix 2 all langevin {panda.tStart} {panda.tStop} 0.1 102938")
     # Update tStart to be the thermal endpoint of last simulation. This approach
     # might lead to some funkiness if tStop was not reached in previous simulation
-    self.tStart = self.tStop
+    panda.tStart = panda.tStop
 
-def extractThermo(self):
+def extractThermo(panda):
     # Store thermo data from last simulation
-    last_thermo = self.lmp.last_thermo()
+    last_thermo = panda.lmp.last_thermo()
     cull = False
     # Check if there's already max amount of data stored, if so enable cull flag to delete the first entry in each keyword
-    if len(self.sim_info["Step"]) >= self.info_size:
+    if len(panda.sim_info["Step"]) >= panda.info_size:
         cull = True
     for key in last_thermo:
         if cull:
-            del self.sim_info[key][0]
-        self.sim_info[key].append(last_thermo[key])
+            del panda.sim_info[key][0]
+        panda.sim_info[key].append(last_thermo[key])
 
-def createPlots(self):
-    fig, ax = plt.subplots()
-    graph = ax.plot(self.sim_info["Step"], self.sim_info["Temp"], color='b')[0]
-    plt.show()
-    return graph, fig , ax
 
-def updatePlots(self):
-    self.graph.set_xdata(self.sim_info["Step"])
-    self.graph.set_ydata(self.sim_info["Temp"])
-    print(self.graph.get_ydata(),"\n\n\n\n\n\n\n\n\n\n\n\n")
-    self.ax.relim()
-    self.ax.autoscale_view()
-    self.fig.canvas.draw()
-    self.fig.canvas.flush_events()
-    plt.pause(1/60)
 
 
 
--- src/simulation.py
+++ src/simulation.py
@@ -1,32 +1,42 @@
-import time
-from math import pi, sin, cos
-from random import randrange
+import sys, time
+from PyQt6 import QtWidgets, QtCore, QtGui
+import pyqtgraph as pg
 from direct.showbase.ShowBase import ShowBase
 from direct.task import Task
 from direct.gui.DirectGui import *
 # Change this panda3d.core import to be more specific
 from panda3d.core import *
+from math import pi, sin, cos
+from random import randrange
 from lammps import lammps, LMP_TYPE_VECTOR, LMP_STYLE_ATOM, LMP_TYPE_ARRAY
 import numpy as np
 from funcs import *
-import wx
-import matplotlib
-import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
-from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg as FigureCanvas
-from matplotlib.backends.backend_wx import NavigationToolbar2Wx
-from matplotlib.figure import Figure
 
-# Used for graphing later
-#matplotlib.use('WXAgg')
+# Offscreen Panda3D config
+load_prc_file_data("", "window-type offscreen")
+load_prc_file_data("", "gl-force-software true")
 
+class OffscreenPanda(ShowBase):
+    def __init__(self, W, H):
+        super().__init__()
+        self.W, self.H = W, H
 
-class MyApp(ShowBase):
-    def __init__(self):
-        ShowBase.__init__(self)
+        # Offscreen buffer & texture
+        buf = self.win.make_texture_buffer("buf", W, H, to_ram=True)
+        self.tex = buf.get_texture()
+        buf.add_render_texture(self.tex, GraphicsOutput.RTMCopyRam, GraphicsOutput.RTPColor)
 
-        # Flags
-        self.pause_flag = False
+        # Offscreen camera
+        dr = buf.get_display_region(0)
+        cam2 = self.make_camera(buf)
+        dr.set_camera(cam2)
+        lens = PerspectiveLens()
+        #lens.set_fov(90)
+        cam2.node().set_lens(lens)
+        cam2.set_pos(4, -25, 3)
+        self.cam2 = cam2
+        self.cam_h = cam2.get_h()
+        self.cam_p = cam2.get_p()
 
         # Create lammps object and get initial coords
         self.lmp = lammps()
@@ -34,8 +44,8 @@ class MyApp(ShowBase):
         self.x = self.lmp.numpy.extract_atom("x")
         self.ix = self.lmp.numpy.extract_compute("compute_ix", LMP_STYLE_ATOM, LMP_TYPE_ARRAY)
         self.xu = self.lmp.numpy.extract_compute("compute_xu", LMP_STYLE_ATOM, LMP_TYPE_ARRAY)
-        self.cell = np.zeros((3,3))
-        self.timestep = 10
+        self.cell = np.zeros((3, 3))
+        self.timestep = 1
         self.tStart = 1
         self.tStop = 1
         # How many iterations of thermo info to be stored before deleting old ones
@@ -49,69 +59,31 @@ class MyApp(ShowBase):
         self.atom_ids = self.lmp.numpy.extract_atom("id")
 
         # animTime determines how long each animation step takes
-        self.animTime = 1/60
+        self.animTime = 1 / 60
 
         # Add templates for different atoms. Add more or change values depending on amount of atoms in simulation
         self.atom_types = {1: {"color": [0.9, 0.9, 0.9], "scale": [0.1, 0.1, 0.1]},
-                           2: {"color": [0.9, 0.0, 0.0], "scale": [0.15, 0.15, 0.15]}}
+                           2: {"color": [0.0, 0.0, 0.9], "scale": [0.15, 0.15, 0.15]}}
         self.atom_type_list = self.lmp.numpy.extract_atom("type")
 
-        # Create pointlight to make atom details visible
-        plight1 = PointLight('plight1')
-        plight1.setColor((0.7, 0.7, 0.7, 1))
-        plnp1 = self.render.attachNewNode(plight1)
-        plnp1.setPos(100, 100, 100)
-        self.render.setLight(plnp1)
+        # Build scene
+        self.render.set_shader_auto()
 
-        # Create second pointlight to make the "dark side" brighter
-        plight2 = PointLight('plight2')
-        plight2.setColor((0.7, 0.7, 0.7, 1))
-        plnp2 = self.render.attachNewNode(plight2)
-        plnp2.setPos(-100, -100, -100)
-        self.render.setLight(plnp2)
+        amb = AmbientLight("amb"); amb.set_color(Vec4(0.7,0.7,0.7,1))
+        self.render.set_light(self.render.attach_new_node(amb))
+        dlight = DirectionalLight("dir"); dlight.set_color(Vec4(1,1,1,1))
+        dnp = self.render.attach_new_node(dlight); dnp.set_hpr(45,-45,0)
+        self.render.set_light(dnp)
 
-        # Create ambientlight to make things more visible
-        alight = AmbientLight('alight')
-        alight.setColor((0.4, 0.4, 0.4, 1))
-        alnp = self.render.attachNewNode(alight)
-        self.render.setLight(alnp)
+        # Spin state
+        self.paused = False
+        self._prev  = self.taskMgr.globalClock.get_frame_time()
 
+
+    def createAtomsTask(self):
         print("Creating atoms...")
-        # Add the createAtomsTask to task manager
-        self.taskMgr.add(self.createAtomsTask, "CreateAtomsTask")
-
-        # Add the moveAtomsTask to task manager
-        self.taskMgr.add(self.moveAtomsTask, "MoveAtomsTask")
-
-        # Add the drawSimulationBoxTask to task manager
-        self.taskMgr.add(self.drawSimulationBoxTask, "DrawSimulationBoxTask")
-
-        print("Setting up the camera...")
-        base.disableMouse()
-        self.camera.setPos(7, -20, 3)
-
-        # Generate GUI objects
-        self.generateGUI()
-
-        # Create initial plots
-        self.graph , self.fig, self.ax = createPlots(self)
-
-
-
-    def generateGUI(self):
-        self.guiFrame = DirectFrame(frameColor=(0.5, 0.5, 0.5, 1), frameSize=(-0.5, 0.5, 0, 2), pos=(1.33, 0, -1))
-        self.start_stop_button = DirectButton(text="Pause", scale=0.1, command=startStopSimulation, extraArgs=[self], textMayChange=1, parent=self.guiFrame, pos=(0.1, 0, 0.05))
-        self.animTime_slider = DirectSlider(value=1, range=(1, 10), pageSize=0.1, command=changeSpeed, extraArgs=[self],
-                                            parent=self.guiFrame, pos=(-0.1, 0, 1.9), scale=0.35)
-        self.thermo_slider = DirectSlider(value=-5, range=(-5, 5), pageSize=0.1, command=changeThermo, extraArgs=[self],
-                                            parent=self.guiFrame, pos=(-0.1, 0, 1.7), scale=0.35)
-        self.thermo_label = DirectLabel(text="Thermostat:", parent=self.guiFrame, scale=0.05, pos=(-0.315, 0, 1.75), frameColor=(0.5, 0.5, 0.5, 1))
-        self.animTime_label = DirectLabel(text="Simulation Speed:", parent=self.guiFrame, scale=0.05, pos=(-0.25, 0, 1.95), frameColor=(0.5, 0.5, 0.5, 1))
-
-
-    def createAtomsTask(self, task):
         for atom_id in self.atom_ids:
-            # Load atom model. If simulation has a lot of atoms or needs to run very quickly,
+            # Load atom model. If simulation has a lot of atoms or needs to run very quickly,l
             # change the model to a lower poly version, which can be found online. Any .egg file should work.
             atom = self.loader.loadModel('../models/Sphere_HighPoly.egg')
             # Reparent to render (important to do this so the model can be rendered)'
@@ -130,7 +102,7 @@ class MyApp(ShowBase):
         return Task.done
 
 
-    def drawSimulationBoxTask(self, task):
+    def drawSimulationBoxTask(self):
         lines = LineSegs()
 
         # bottom face
@@ -165,23 +137,19 @@ class MyApp(ShowBase):
         return Task.done
 
 
-    def moveAtomsTask(self, task):
-        print("Moving atoms")
-        if self.pause_flag == False:
+    def moveAtomsTask(self):
+        print("Moving atoms...")
+        if not panda.paused:
             starttime = time.monotonic()
             self.run_single()
-
             for i in range(self.atom_count):
                 new_pos = (self.ix[i,:]-self.ix_old[i,:]) @ self.cell + self.x[i,:]
-
-                posInterval = self.atoms[i].posInterval(self.animTime, Point3(new_pos[0], new_pos[1], new_pos[2]),
-                                               Point3(self.x_old[i,0], self.x_old[i,1], self.x_old[i,2]))
-                posInterval.start()
-            self.taskMgr.doMethodLater(self.animTime - (time.monotonic() - starttime), self.moveAtomsTask, "MoveAtomsTask")
+                self.atoms[i].setPos(new_pos[0], new_pos[1], new_pos[2])
         return Task.done
 
 
     def run_single(self):
+        print("Running single...")
         # store old values for reference
         self.x_old = self.x.copy()
         self.ix_old = self.ix.copy()
@@ -192,9 +160,6 @@ class MyApp(ShowBase):
 
         # Store thermo info for graphing
         extractThermo(self)
-
-        # Update graphs
-        updatePlots(self)
 
         boxlo, boxhi, xy, yz, xz, periodicity, box_change = self.lmp.extract_box()
         cell = np.zeros((3,3))
@@ -222,8 +187,154 @@ class MyApp(ShowBase):
         self.xu = xu_sorted
 
 
+    def rotate_camera(self, dx, dy):
+        """Called from mouse drag to orbit the offscreen camera."""
+        self.cam_h = (self.cam_h - dx*0.2) % 360
+        self.cam_p = max(-85, min(85, self.cam_p + dy*0.2))
+        self.cam2.set_hpr(self.cam_h, self.cam_p, 0)
+
+
+    def zoom_camera(self, delta):
+        """Zoom in/out by moving the camera along its Y-axis."""
+        cam = self.cam2
+        y = cam.get_y()
+        y = max(-50, min(-2, y + delta * 0.2))  # clamp between -50 and -2
+        cam.set_y(y)
+
+
+    def render_frame_to_qimage(self):
+        # 1) Advance spin
+        now = self.taskMgr.globalClock.get_frame_time()
+        dt  = now - self._prev
+        self._prev = now
+
+        # 2) Render & extract
+        self.graphicsEngine.render_frame()
+        self.graphicsEngine.extract_texture_data(self.tex, self.win.get_gsg())
+
+        # 3) Grab bytes and wrap in QImage
+        ram = self.tex.get_ram_image()
+        raw = ram.get_data()   # RGBA8
+        bpl = 4 * self.W
+        img = QtGui.QImage(raw, self.W, self.H, bpl, QtGui.QImage.Format.Format_RGBA8888)
+        return img.mirrored(False, True)
+
+
+class PandaLabel(QtWidgets.QLabel):
+    """A QLabel that forwards left-drag deltas to OffscreenPanda.rotate_camera."""
+    def __init__(self, panda: OffscreenPanda, parent=None):
+        super().__init__(parent)
+        self.panda = panda
+        self.setMouseTracking(True)
+        self._last = None
+
+
+    def mousePressEvent(self, ev: QtGui.QMouseEvent):
+        if ev.buttons() & QtCore.Qt.MouseButton.LeftButton:
+            self._last = ev.position()
+
+
+    def mouseMoveEvent(self, ev: QtGui.QMouseEvent):
+        if self._last and (ev.buttons() & QtCore.Qt.MouseButton.LeftButton):
+            curr = ev.position()
+            dx = curr.x() - self._last.x()
+            dy = curr.y() - self._last.y()
+            self.panda.rotate_camera(dx, dy)
+            self._last = curr
+
+
+    def mouseReleaseEvent(self, ev: QtGui.QMouseEvent):
+        self._last = None
+
+
+    def wheelEvent(self, event: QtGui.QWheelEvent):
+        delta = event.angleDelta().y() / 120  # 1 unit per notch
+        self.panda.zoom_camera(delta)  # negative to zoom in on scroll up
+
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self, panda: OffscreenPanda):
+        super().__init__()
+        self.panda = panda
+        self.setWindowTitle("Interactive Panda3D + PyQt6")
+
+        # Layouts
+        central = QtWidgets.QWidget()
+        hbox = QtWidgets.QHBoxLayout(central)
+
+        # Sidebar
+        sidebar = QtWidgets.QWidget()
+        sidebar.setMinimumWidth(500)
+        vbox = QtWidgets.QVBoxLayout(sidebar)
+        self.btn = QtWidgets.QPushButton("Pause")
+        self.btn.clicked.connect(self.toggle_play)
+        vbox.addWidget(self.btn)
+        vbox.addWidget(QtWidgets.QLabel("Simulation Speed:"))
+        self.sld = QtWidgets.QSlider(QtCore.Qt.Orientation.Horizontal)
+        self.sld.setRange(1, 10)
+        self.sld.valueChanged.connect(lambda v: changeSpeed(panda, v))
+        vbox.addWidget(self.sld)
+        vbox.addWidget(QtWidgets.QLabel("Thermostat:"))
+        self.sld = QtWidgets.QSlider(QtCore.Qt.Orientation.Horizontal)
+        self.sld.setRange(-5, 5)
+        self.sld.valueChanged.connect(lambda v: changeThermo(panda, v))
+        vbox.addWidget(self.sld)
+        self.graph = pg.PlotWidget(title="Temperature over Time")
+        self.graph.setLabel('left','Temperature')
+        self.graph.setLabel('bottom','Time','s')
+        self.curve = self.graph.plot(pen='y')
+        self.xdata, self.ydata = [], []
+        self.start = time.time()
+        vbox.addWidget(self.graph)
+        vbox.addStretch(1)
+
+        # Panda image label
+        self.label = PandaLabel(panda)
+        self.label.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
+        self.label.setMinimumSize(panda.W, panda.H)
+
+        # Assemble
+        hbox.addWidget(sidebar)
+        hbox.addWidget(self.label, 1)
+        self.setCentralWidget(central)
+        self.resize(panda.W + 250, panda.H)
+
+        # Frame timer
+        timer = QtCore.QTimer(self)
+        timer.timeout.connect(self.update_frame)
+        timer.start(33)
+
+
+    @QtCore.pyqtSlot()
+    def update_frame(self):
+        if not self.panda.paused:
+            # Run a simulation step
+            panda.moveAtomsTask()
+            # Draw Panda frame
+            qimg = self.panda.render_frame_to_qimage()
+            self.label.setPixmap(QtGui.QPixmap.fromImage(qimg))
+            # Update graph
+            t = time.time() - self.start
+            extractThermo(panda)
+            self.xdata, self.ydata = panda.sim_info["Step"], panda.sim_info["Temp"]
+            if len(self.xdata) > 300:
+                self.xdata.pop(0); self.ydata.pop(0)
+            self.curve.setData(self.xdata, self.ydata)
+
+
+    def toggle_play(self):
+        self.panda.paused = not self.panda.paused
+        self.btn.setText("Play" if self.panda.paused else "Pause")
+
 
 if __name__ == "__main__":
-    app = MyApp()
-    app.run()
+    W, H = 960, 720
+    panda = OffscreenPanda(W, H)
+    panda.createAtomsTask()
+    panda.moveAtomsTask()
+    panda.drawSimulationBoxTask()
 
+    app = QtWidgets.QApplication(sys.argv)
+    win = MainWindow(panda)
+    win.show()
+    sys.exit(app.exec())
